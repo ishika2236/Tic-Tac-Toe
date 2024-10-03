9fa5e74e3dbccd79936ebf077e1389f76c0b0c5e
ishika2236 <2036.ishika@gmail.com>
2024-10-03 15:56:18 +0530

    backend code updated

---
+++ Backend/Controllers/match.js
@@ -0,0 +1,18 @@
+const Match = require('../Models/match')
+
+const getMatchHistory = async(req,res)=>{
+    try {
+        const matches = await Match.find({
+            $or :[
+                { playerX: req.user.id },
+                { playerO: req.user.id }
+            ]
+        }).populate('playerX playerO', username).sort({date: -1});
+
+        res.json(matches);
+        
+    } catch (error) {
+        res.status(500).json({message: 'Server error'});
+        
+    }
+}
---
+++ Backend/Models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    matchesPlayed: { type: Number, default: 0 },
+    matchesWon: { type: Number, default: 0 },
+    matchesLost: { type: Number, default: 0 },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
